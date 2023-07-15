e0778b325aeea623b29ea5d60500747d6e83a421
Sanja <74556703+SanjaRogovic@users.noreply.github.com>
2023-07-15 12:15:20 +0200

    edited js

--- index.js
+++ index.js
@@ -1,135 +1,138 @@
-//// UPDATED JS
+class ToDoManager {
+    constructor(){
+        this.taskArray = [];
+        this.taskList = document.querySelector('#task-list');
+        this.form = document.querySelector('#task-form');
+        this.input = document.querySelector('#task-input');
+        this.form.addEventListener('submit', this.addTask.bind(this));  
+    }
 
-
-// class ToDoManager {
-//     constructor(){
-//         this.#taskArray = [];
-//         this.#taskList = document.querySelector('#tasks');
-//     }
-
-//     get taskArray(){
-//         return this.#taskArray;
-//     }
-
-//     get taskList(){
-//         return this.#taskList;
-//     }
-// }
-
-
-// class TaskItem {
-//     constructor(){
-//         this.#taskName = taskName;
-//         this.#taskDone = false;
-        
-//        
-//     }
-
-//     get taskName(){
-//         return this.#taskName;
-//     }
-
-//     get taskDone(){
-//         return this.#taskDone;
-//     }
-
-//     addTaskItem(){
-//         const taskInput = document.querySelector('#task-input');
-//         taskName = taskInput.value;
-//     }
-
-//     markChecked(){
-//         this.#taskDone = true;
-        
-//     }
-
-    
-            
-        
-
-       
-//     }
-
-//     deleteTaskItem(task){
-//         this.#thisArray.removeChild(task)
-//         const deleteButton = document.querySelector('#delete');
-//         deleteButton.innerText = 'DELETE';
-//         taskItem.appendChild(deleteButton);
-//         deleteButton.addEventListener('click', deleteTaskItem) 
-
-//     }
-// }
-
-
-
-
-// const toDo = new ToDoManager();
-// const newTask = new TaskItem()
-
-const form = document.querySelector('#task-form');
-const input = document.querySelector('#task-input');
-let taskList = document.querySelector('#task-list');
-
-
-form.addEventListener('submit', (event) => {
+addTask(event){
     event.preventDefault();
-
-    if (input.value.trim() === "") {
+    if (this.input.value.trim() === "") {
         alert("Please Enter a Task");
     } else {
-        const newTask = document.createElement('li')
-        newTask.innerText = input.value;
-        // newTask.classList.add('div'); //CSS styling
-        taskList.appendChild(newTask);
-        input.value = "";
+        const newTaskTemplate = `<li>
+        <div class = "taskElement">${this.input.value}</div>
+        <button class = "edit-button">Edit</button>
+        <button class = "delete-button">Delete</button>
+        <input type = "checkbox"</>
+        </li>`
+        this.taskList.innerHTML += newTaskTemplate;
+        this.input.value = "";
 
-        const newTaskActions = document.querySelector('ul'); //creates and stores task actions(delete, edit buttons)
-        // newTaskActions.classList.add('actions'); // CSS styling
-        newTaskActions.appendChild(newTask);
+                 
 
+const toDoItem = new TaskItem(this.input.value);
+this.taskArray.push(toDoItem);
 
-        // creating DELETE button
-        const deleteButton = document.createElement('button');
-        deleteButton.innerText = 'DELETE';
-        newTask.appendChild(deleteButton);
-        deleteButton.addEventListener('click', deleteTask) 
-
-        // function that executes DELETE
-        function deleteTask(){
-            taskList.removeChild(newTask);
-            
-        }  
-
-        // creating EDIT button
-        const editButton = document.createElement('button');
-        editButton.innerText = 'EDIT';
-        newTask.appendChild(editButton);
-        let inputTask;
-        editButton.addEventListener('click', () => {
-            if(editButton.innerText === 'EDIT'){
-                editButton.innerText = 'SAVE' 
-                inputTask = document.createElement('input');
-                newTask.appendChild(inputTask) 
-                } else {
-                    if(editButton.innerText === 'SAVE'){
-                        if (inputTask.value.trim() === "") {
-                            alert("Please edit a Task");
-                        } else{
-                            console.log(inputTask.value);
-                            newTask.innerText = inputTask.value;
-                            inputTask.remove();
-                            editButton.innerText = 'EDIT'; 
-                            newTask.appendChild(deleteButton);
-                            newTask.appendChild(editButton);
-                        }   
-                    }
-                }
-                               
-             }) 
-                    
+};
 }
 
-})
+
+// editTask(){
+    // event.preventDefault();
+    // const editButton = document.querySelector('#edit-button');
+    // editButton.addEventListener('click',() => {
+    //     editButton.textContent = ""
+    // // document.getElementByClass("task-element").innerHTML = "";
+
+    // })
+
+}
+
+
+//this.form.addEventListener('submit', this.addTask.bind(this));
+
+
+class TaskItem{
+    constructor(taskName){
+        this.taskName = taskName;
+        //this.#taskDone = false;
+    }
+
+//   editTask(value){
+//     this.taskName = value;
+//   }
+
+deleteTask(){
+    document.getElementsByClassName('delete-button').addEventListener('click', (event) => {
+        event.preventDefault();
+        newTaskTemplate.remove('li');
+    })
+}
+};
+
+
+const toDo = new ToDoManager();
+const newTask = new TaskItem()
+
+
+
+
+
+
+
+
+// form.addEventListener('submit', (event) => {
+//     event.preventDefault();
+
+//     if (input.value.trim() === "") {
+//         alert("Please Enter a Task");
+//     } else {
+//         const newTask = document.createElement('li')
+//         newTask.innerText = input.value;
+//         // newTask.classList.add('div'); //CSS styling
+//         taskList.appendChild(newTask);
+//         input.value = "";
+
+//         const newTaskActions = document.querySelector('ul'); //creates and stores task actions(delete, edit buttons)
+//         // newTaskActions.classList.add('actions'); // CSS styling
+//         newTaskActions.appendChild(newTask);
+
+
+//         // creating DELETE button
+//         const deleteButton = document.createElement('button');
+//         deleteButton.innerText = 'DELETE';
+//         newTask.appendChild(deleteButton);
+//         deleteButton.addEventListener('click', deleteTask) 
+
+//         // function that executes DELETE
+//         function deleteTask(){
+//             taskList.removeChild(newTask);
+            
+//         }  
+
+//         // creating EDIT button
+//         const editButton = document.createElement('button');
+//         editButton.innerText = 'EDIT';
+//         newTask.appendChild(editButton);
+//         let inputTask;
+//         editButton.addEventListener('click', () => {
+//             if(editButton.innerText === 'EDIT'){
+//                 editButton.innerText = 'SAVE' 
+//                 inputTask = document.createElement('input');
+//                 newTask.appendChild(inputTask) 
+//                 } else {
+//                     if(editButton.innerText === 'SAVE'){
+//                         if (inputTask.value.trim() === "") {
+//                             alert("Please edit a Task");
+//                         } else{
+//                             console.log(inputTask.value);
+//                             newTask.innerText = inputTask.value;
+//                             inputTask.remove();
+//                             editButton.innerText = 'EDIT'; 
+//                             newTask.appendChild(deleteButton);
+//                             newTask.appendChild(editButton);
+//                         }   
+//                     }
+//                 }
+                               
+//              }) 
+                    
+// }
+
+// })
 
 
 
